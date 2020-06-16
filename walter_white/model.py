8115bc1eff26e13d7b9b2ac779177bb8196a6ab9
Daniel Schruhl <danielschruhl@gmail.com>
2020-06-16 22:25:46 +0200

    fix linter

--- walter_white/model.py
+++ walter_white/model.py
@@ -1,11 +1,10 @@
 from functools import reduce
 
-from tensorflow import keras
-from tensorflow.keras import layers
+import tensorflow as tf
 
 
 def _compile_layer(acc, layer_config):
-    return layers.Dense(
+    return tf.keras.layers.Dense(
         layer_config['neurons'],
         activation=layer_config['activationFunction']
     )(acc)
@@ -13,11 +12,11 @@ def _compile_layer(acc, layer_config):
 
 def autoencoder(neural_net_config):
     nn_layers = neural_net_config['layers']
-    input_layer = keras.Input(shape=(nn_layers['input']['neurons'],), name='input')
+    input_layer = tf.keras.Input(shape=(nn_layers['input']['neurons'],), name='input')
     stacks = reduce(_compile_layer, nn_layers['stacks'], input_layer)
-    output_layer = layers.Dense(
+    output_layer = tf.keras.layers.Dense(
         nn_layers['output']['neurons'],
         activation=nn_layers['output']['activationFunction'],
         name='output'
     )(stacks)
-    return keras.Model(inputs=input_layer, outputs=output_layer)
+    return tf.keras.Model(inputs=input_layer, outputs=output_layer)
