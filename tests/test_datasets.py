e3accb03c873d5d324c6f97335d375b2f2f0a9b0
Daniel Schruhl <danielschruhl@gmail.com>
2020-06-17 12:18:25 +0200

    Add normalization (division by dictionary size) for word vectors

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -5,7 +5,7 @@ from walter_white import datasets
 
 
 def test_should_load_dataset_from_one_file():
-    actual_dataset = datasets.load_dataset('tests/resources/test.csv')
+    actual_dataset = datasets.load_dataset('tests/resources/test.csv', 1)
     actual = next(iter(actual_dataset.batch(3)))
 
     npt.assert_array_equal(actual, np.array([
@@ -16,16 +16,16 @@ def test_should_load_dataset_from_one_file():
 
 
 def test_should_load_dataset_from_directory():
-    actual_dataset = datasets.load_dataset('tests/resources/*.csv')
+    actual_dataset = datasets.load_dataset('tests/resources/*.csv', 10)
     actual = next(iter(actual_dataset.batch(8)))
 
     npt.assert_array_equal(actual, np.array([
-        [25.0, 26.0, 22.0],
-        [25.0, 25.0, 1.0],
-        [25.0, 21.0, 2.0],
-        [25.0, 25.0, 23.0],
-        [1.0, 26.0, 22.0],
-        [33.0, 25.0, 1.0],
-        [42.0, 21.0, 2.0],
-        [2.0, 25.0, 23.0],
+        [2.5, 2.6, 2.2],
+        [2.5, 2.5, 0.1],
+        [2.5, 2.1, 0.2],
+        [2.5, 2.5, 2.3],
+        [0.1, 2.6, 2.2],
+        [3.3, 2.5, 0.1],
+        [4.2, 2.1, 0.2],
+        [0.2, 2.5, 2.3],
     ]))
--- walter_white/datasets.py
+++ walter_white/datasets.py
@@ -7,13 +7,13 @@ import tensorflow as tf
 LOG = logging.getLogger(__name__)
 
 
-def parse_line(line):
+def parse_line(dictionary_size, line):
     split_line = tf.strings.split(line, sep=',')
-    return tf.strings.to_number(split_line, out_type=tf.dtypes.double)
+    return tf.divide(tf.strings.to_number(split_line, out_type=tf.dtypes.double), dictionary_size)
 
 
-def load_dataset(dataset_path):
+def load_dataset(dataset_path, dictionary_size):
     absolute_dataset_path = os.path.realpath(dataset_path)
     all_files = tf.data.Dataset.list_files(absolute_dataset_path, shuffle=False)
     raw_lines_dataset = tf.data.TextLineDataset(all_files)
-    return raw_lines_dataset.map(partial(parse_line))
+    return raw_lines_dataset.map(partial(parse_line, dictionary_size))
