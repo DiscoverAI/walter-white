6ec0304e96602480458c46e047c7ac365abfb6ea
Daniel Schruhl <danielschruhl@gmail.com>
2020-07-19 22:04:17 +0200

    Less epochs

--- walter_white/train.py
+++ walter_white/train.py
@@ -10,7 +10,7 @@ LOG = logging.getLogger(__name__)
 NN_CONF = {
     "optimizer": "adadelta",
     "lossFunction": "binary_crossentropy",
-    "epochs": 30,
+    "epochs": 15,
     "batchSize": 10000,
     "layers": {
         "input": {
